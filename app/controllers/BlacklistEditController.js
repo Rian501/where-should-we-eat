181be8ba7ba458640bff961a833743c2194d2ac8
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-08-17 10:46:56 -0500

    separate the faves and blacklist, and style login page

--- app/controllers/BlacklistEditController.js
+++ app/controllers/BlacklistEditController.js
@@ -7,10 +7,6 @@ eatsApp.controller('BlacklistEditController', function ($scope, $window, $routeP
 		$scope.blacklistArray = blacklist;
 	});
 
-	SuggestionsFactory.getSavedlist(UserFactory.getUser())
-	.then( (savedlist) => {
-		$scope.savedListArray = savedlist;
-	});
 
 	$scope.removeFromBlacklist = (FBid) => {
 		console.log("item to remove", FBid);
@@ -20,13 +16,6 @@ eatsApp.controller('BlacklistEditController', function ($scope, $window, $routeP
 		});
 	};
 
-	$scope.removeFromSaved = (FBid) => {
-		console.log("item to remove", FBid);
-		SuggestionsFactory.removeFromSavelist(FBid)
-		.then( (response) => {
-			$window.location.reload();
-		});
-	};
 
 
 });
